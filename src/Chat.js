c854c571659436544abe06a87ae3442a9d2afa3e
rv <rv@rv.com>
2022-04-26 11:18:34 +0530

    day1

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Chat.css';
+
+function Chat() {
+  return (
+    <div>
+        <h1>Hello</h1>
+
+      
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/Select_bar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './select_bar.css'
+function SelectBar() {
+  return (
+    <div className="selectbar">
+      <h1>select bar</h1>
+    </div>
+  )
+}
+
+export default SelectBar
